06430b4aa35a731da55292616ef470b958f9a9cf
alanbrunoscience <alanbrunomath@gmail.com>
2024-12-04 11:32:59 -0300

    Include more examples

---
+++ Trails/Week 1 - JS First Steps/Examples/ex11_TernaryConditional.js
@@ -0,0 +1,15 @@
+// condição ? expressão_01 (true) : expressão_02 (false)
+
+let idade = 15;
+
+idade > 18
+    ? console.log("O cliente é maior de 18 anos")
+    : console.log("O cliente é menor de 18 anos")
+
+/* 
+if(idade < 18) {
+    console.log("O cliente é maior de 18 anos");
+} else {
+    console.log("O cliente é menor de 18 anos");
+}
+*/
