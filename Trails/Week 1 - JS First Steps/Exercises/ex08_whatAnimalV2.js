fa1b80f04f0ca9c0f439132fa0ff09a35ad8e138
alanbrunoscience <alanbrunomath@gmail.com>
2024-12-05 11:51:48 -0300

    Include more solution exercises

---
+++ Trails/Week 1 - JS First Steps/Exercises/ex08_whatAnimalV2.js
@@ -0,0 +1,60 @@
+/*
+Escreva um algoritmo que leia 3 palavras (string), que definem as características de um tipo de animal, segundo o diagrama abaixo, que deve ser lido da esquerda para a direita. 
+*/
+
+const input = require("readline-sync");
+
+let charac1 = input.question("1) Enter the first characteristic ('vertebrate' or 'invertebrate'): ").toLowerCase();
+
+if(charac1 !== "vertebrate" && charac1 !== "invertebrate") {
+    console.log("\n-> Invalid input! It must be 'vertebrate' or 'invertebrate'.");
+    process.exit(0);
+}
+
+let charac2 = input.question("2) Enter the second characteristic ('bird', 'mammal', 'insect', or 'annelid'): ").toLowerCase();
+
+if(charac2 !== "bird" && charac2 !== "mammal" && charac2 !== "insect" && charac2 !== "annelid") {
+    console.log("\n-> Invalid input! It must be 'bird', 'mammal', 'insect', or 'annelid'.");
+    process.exit(0);
+}
+
+let charac3 = input.question("3) Enter the third characteristic ('carnivore', 'omnivorous', 'herbivore', or 'hematophagous'): ").toLowerCase();
+
+if(charac3 !== "carnivore" && charac3 !== "omnivorous" && charac3 !== "herbivore" && charac3 !== "hematophagous") {
+    console.log("\n-> Invalid input! It must be 'carnivore', 'omnivorous', 'herbivore', or 'hematophagous'.");
+    process.exit(0);
+}
+
+if(charac1 === "vertebrate") {
+    if(charac2 === "bird") {
+        if(charac3 === "carnivore") {
+            console.log("\nThe animal is an eagle.");
+        } else if(charac3 === "omnivorous") {
+            console.log("\nThe animal is a dove.");
+        }
+    } else if(charac2 === "mammal") {
+        if(charac3 === "omnivorous") {
+            console.log("\nThe animal is a man.");
+        } else if(charac3 === "herbivore") {
+            console.log("\nThe animal is a cow.");
+        }
+    } else {
+        console.log("\nAnimal not found!");
+    }
+} else if (charac1 === "invertebrate"){
+    if(charac2 === "insect") {
+        if(charac3 === "hematophagous") {
+            console.log("\nThe animal is a flea.");
+        } else if(charac3 === "herbivore") {
+            console.log("\nThe animal is a caterpillar.");
+        }
+    } else if(charac2 === "annelid") {
+        if(charac3 === "hematophagous") {
+            console.log("\nThe animal is a leech.");
+        } else if(charac3 === "omnivorous") {
+            console.log("\nThe animal is a worm.");
+        }
+    } else {
+        console.log("\nAnimal not found!");
+    }
+}
