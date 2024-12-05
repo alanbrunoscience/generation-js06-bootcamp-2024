b203670ce7151d9b78f0140e04eaccbb632ac2b4
alanbrunoscience <alanbrunomath@gmail.com>
2024-12-05 12:00:03 -0300

    Include Strict Mode explanation

---
+++ Trails/Week 1 - JS First Steps/Examples/ex12_StrictMode.js
@@ -0,0 +1,11 @@
+"use strict" // Mandate variable declaration
+
+// numero = 10; This doesn't work when using "use strict". So, it would help if you used let numero = 10.
+
+let numero = 10;
+
+console.log("O valor de variável número é: ", numero);
+
+numero = 100;
+
+console.log("O valor de variável número é: ", numero);
