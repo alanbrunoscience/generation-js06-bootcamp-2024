e45d78f1838fb696f07b49a788dfcc4f8af6b38b
alanbrunoscience <alanbrunomath@gmail.com>
2024-12-05 23:17:47 -0300

    Include exercises solution

---
+++ Trails/Week 1 - JS First Steps/Exercises/l3_ex06_averageMultipleOf3.js
@@ -0,0 +1,26 @@
+"use strict"
+
+/*
+
+Escreva um algoritmo, que leia números inteiros via teclado, até que o número zero seja digitado. Ao final, mostre na tela a média de todos os números digitados, que sejam múltiplos de 3.
+
+*/
+
+const input = require("readline-sync");
+
+let num, sumMultiple3 = 0, countMult3 = 0, averageMult3;
+
+do {
+
+    num = input.questionInt("\nEnter an integer number: ");
+
+    if((num != 0) && num % 3 === 0) {
+        sumMultiple3 += num;
+        countMult3++;
+    }
+
+} while(num !== 0);
+
+averageMult3 = sumMultiple3 / countMult3;
+
+console.log(`\n-> The average of all numbers that are multiples of 3 is ${averageMult3.toFixed(2)}.`);
