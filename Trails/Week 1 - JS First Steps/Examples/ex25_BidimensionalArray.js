fca0389a424bb3855efc125fab1ba32b1edd8398
alanbrunoscience <alanbrunomath@gmail.com>
2024-12-06 10:28:29 -0300

    Include more examples

---
+++ Trails/Week 1 - JS First Steps/Examples/ex25_BidimensionalArray.js
@@ -0,0 +1,20 @@
+"use strict"
+
+const input = require("readline-sync");
+
+// Creating a bi-dimensional array
+let array2DInt = new Array(3); // Rows
+
+for (let i = 0; i < array2DInt.length; i++){
+    array2DInt[i] = Array(3); // Columns
+}
+
+// Filling up the bi-dimensional array
+for (let i = 0; i < array2DInt.length; i++) {
+    for (let j = 0; j < array2DInt.length; j++) {
+        array2DInt[i][j] = input.questionInt("Enter an integer number, please: ");
+    }
+}
+
+console.log();
+console.table(array2DInt);
