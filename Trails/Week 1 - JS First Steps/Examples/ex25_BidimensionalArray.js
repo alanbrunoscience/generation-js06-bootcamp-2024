52b395589c7a3592e276a1d85822ac8449e96bd9
alanbrunoscience <alanbrunomath@gmail.com>
2024-12-06 10:29:45 -0300

    Include more examples

--- Trails/Week 1 - JS First Steps/Examples/ex25_BidimensionalArray.js
+++ Trails/Week 1 - JS First Steps/Examples/ex25_BidimensionalArray.js
@@ -3,10 +3,10 @@
 const input = require("readline-sync");
 
 // Creating a bi-dimensional array
-let array2DInt = new Array(3); // Rows
+let array2DInt = new Array(3); // Creating Rows
 
 for (let i = 0; i < array2DInt.length; i++){
-    array2DInt[i] = Array(3); // Columns
+    array2DInt[i] = Array(3); // Creating Columns
 }
 
 // Filling up the bi-dimensional array
