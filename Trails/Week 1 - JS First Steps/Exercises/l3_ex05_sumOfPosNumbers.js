4a749b9cd70daa54dabcf01c61d46f7c907f14ad
alanbrunoscience <alanbrunomath@gmail.com>
2024-12-05 23:01:53 -0300

    Include exercises solution

---
+++ Trails/Week 1 - JS First Steps/Exercises/l3_ex05_sumOfPosNumbers.js
@@ -0,0 +1,23 @@
+"use strict"
+
+/*
+
+Escreva um algoritmo, que leia números inteiros via teclado, até que o número zero seja digitado. Ao final, mostre na tela a soma de todos os números digitados, que sejam positivos.
+
+*/
+
+const input = require("readline-sync");
+
+let num, sumPosNum = 0;
+
+do {
+
+    num = input.questionInt("\nEnter an integer number: ");
+
+    if(num >= 0) {
+        sumPosNum += num;
+    }
+
+} while(num !== 0);
+
+console.log(`\n-> The sum of positive numbers is ${sumPosNum}.`);
