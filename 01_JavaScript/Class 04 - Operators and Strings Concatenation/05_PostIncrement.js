4caf851ff41006649dbae19e26ef6f8a5d17c15e
alanbrunoscience <alanbrunomath@gmail.com>
2024-12-08 17:55:25 -0300

    Reorganize the code examples

--- 01_JavaScript/Class 04 - Operators and Strings Concatenation/05_PostIncrement.js
+++ 01_JavaScript/Class 04 - Operators and Strings Concatenation/05_PostIncrement.js
@@ -3,3 +3,9 @@ let num = 1;
 console.log("O valor atual da variável número é: ", num);
 num++;
 console.log("O valor pós-incrementado da variável número é: ", num);
+
+/* 
+
+No Operador de Pós-Incremento, a variável "num" é retornada com o valor original e só depois o seu valor atual é incrementado. Observe que adicionamos um novo console.log() para visualizar o valor atualizado da variável "num".
+
+*/
