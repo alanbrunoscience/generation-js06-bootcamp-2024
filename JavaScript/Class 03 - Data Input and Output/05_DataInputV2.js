ac1b099f2c7178239e86620fea53ed1e212d5a1c
alanbrunoscience <alanbrunomath@gmail.com>
2024-12-08 17:11:51 -0300

    Reorganize the code examples

--- JavaScript/Class 03 - Data Input and Output/05_DataInputV2.js
+++ JavaScript/Class 03 - Data Input and Output/05_DataInputV2.js
@@ -1,6 +1,6 @@
 /*
 
-O Pacote Readline-Sync fornece uma maneira fácil de lidar com entradas inválidas nos Métodos questionInt() e questionFloat(). Você pode usar a opção limitMessage para personalizar a mensagem de erro quando o usuário insere um valor inválido.
+- O Pacote Readline-Sync fornece uma maneira fácil de lidar com entradas inválidas nos Métodos questionInt() e questionFloat(). Você pode usar a opção limitMessage para personalizar a mensagem de erro quando o usuário insere um valor inválido.
 
 */
 
@@ -28,3 +28,9 @@ console.log("\n-> The boolean value typed was: " + answer);
 
 word = input.question("\nEnter a string value: ");
 console.log("\n-> The string value typed was: " + word);
+
+/*
+
+- Na entrada de dados das variáveis do tipo ponto flutuante (números reais), ao digitar os valores, observe que os números decimais foram separados por ponto e não por vírgula. A explicação é simples: O JavaScript utiliza o padrão Americano, onde o separador de decimais é o ponto. Caso as casas decimais sejam zero, não é necessário digitar .00, basta digitar o número como se fosse um número inteiro. Caso você se esqueça e digite vírgula, ao invés do ponto, Pacote Readline-Sync irá ignorar a parte decimal e receberá apenas a parte inteira.
+
+*/
