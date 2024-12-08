5dd5d699379e8e26da8934ae18e36e9bb7667ea8
alanbrunoscience <alanbrunomath@gmail.com>
2024-12-08 20:39:35 -0300

    Reorganize the code examples

--- 01_JavaScript/Class 06 - Loops/02_ForLoop.js
+++ 01_JavaScript/Class 06 - Loops/02_ForLoop.js
@@ -6,6 +6,7 @@ let num, i;
 
 num = input.questionInt("Enter an integer number: ");
 
+console.log(`\n===== Multiplication Table of ${num} =====\n`);
 for(i = 1; i <= 10; i++) {
     console.log(num + " * " + i + " = " + (num * i));
 }
