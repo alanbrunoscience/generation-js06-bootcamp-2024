1338c20e0f72a28555055f9f4a720e4c416c20bc
alanbrunoscience <alanbrunomath@gmail.com>
2024-12-08 19:38:15 -0300

    Reorganize the code examples

---
+++ 01_JavaScript/Class 05 - Conditional Structures/02_SwitchCaseIf.js
@@ -0,0 +1,32 @@
+/* SWITCH CASE como IF
+
+- O JavaScript, diferentemente de outras linguagens, consegue implementar um SWITCH CASE de forma similar a um IF ELSE, mas com algumas diferenças no modo de operação.
+
+*/
+
+"use strict"
+
+const leia = require('readline-sync')
+
+let categoria;
+
+let idade = leia.questionInt("Digite a idade do cliente: ");
+
+switch (true) {
+  case idade >= 0 && idade <= 12:
+    categoria = "Criança";
+    break;
+  case idade >= 13 && idade <= 17:
+    categoria = "Adolescente";
+    break;
+  case idade >= 18 && idade <= 59:
+    categoria = "Adulto";
+    break;
+  case idade >= 60:
+    categoria = "Idoso";
+    break;
+  default:
+    categoria = "Idade inválida";
+}
+
+console.log(`\n-> O Cliente é ${categoria}`);
