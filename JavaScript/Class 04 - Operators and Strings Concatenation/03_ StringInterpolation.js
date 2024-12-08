13286a1641d0d587ff19fd67c8b2923ff8870294
alanbrunoscience <alanbrunomath@gmail.com>
2024-12-08 17:46:59 -0300

    Reorganize the code examples

---
+++ JavaScript/Class 04 - Operators and Strings Concatenation/03_ StringInterpolation.js
@@ -0,0 +1,6 @@
+"use strict"
+
+let submittedActivities = 90.5, attendance = 100.0;
+
+console.log(`The participant had ${submittedActivities}% of submitted activities;`);
+console.log(`The participant was ${attendance}% in the sections.`);
