3e9207925b2fc8887ae36e658b9fdb2fae08be0b
alanbrunoscience <alanbrunomath@gmail.com>
2024-12-08 13:28:41 -0300

    Organize folder and rename files

--- JavaScript/Class 03 - Data Input and Output/01_DisplayDataOnScreen.js
+++ JavaScript/Class 03 - Data Input and Output/01_DisplayDataOnScreen.js
@@ -7,4 +7,15 @@ let identifier = 10000;
 let height = 25.4;
 let area = 45.4567;
 let char = 'A';
-let type = ;
+let answer = true;
+let word = "Generation";
+
+console.log(bit);
+console.log(value);
+console.log(quantity);
+console.log(identifier);
+console.log(height);
+console.log(area);
+console.log(char);
+console.log(answer);
+console.log(word);
