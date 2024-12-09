92bd48c1a6a0874fc627a9f53b2295297eeb089e
alanbrunoscience <alanbrunomath@gmail.com>
2024-12-09 14:53:02 -0300

    Insert TS examples codes

---
+++ 02_TypeScript/Class 02 - Variables and Constants/06_Constants.ts
@@ -0,0 +1,20 @@
+/* Declaração de Constantes
+
+- No JavaScript, a declaração de uma constante é bem parecida com a declaração de uma variável. Basta colocar a palavra reservada "const" antes do identificador da variável.
+
+*/
+
+const quantity: number = 10;
+console.log(quantity);
+
+const myName: string = "Mariana Alves";
+console.log(myName);
+
+const answer: boolean = true;
+console.log(answer);
+
+/*
+
+- Uma Constante, diferente de uma Variável, não pode ser declarada sem a atribuição de um valor de inicialização e, além disso, não pode ter o seu valor alterado, ou seja, ela é imutável.
+
+*/
