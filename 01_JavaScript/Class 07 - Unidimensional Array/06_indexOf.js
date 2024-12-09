dd365f1d53d04ffb20fc6c9d4ece5de71ce94ffe
alanbrunoscience <alanbrunomath@gmail.com>
2024-12-08 22:31:38 -0300

    Reorganize the code examples

---
+++ 01_JavaScript/Class 07 - Unidimensional Array/06_indexOf.js
@@ -0,0 +1,7 @@
+"use strict"
+
+let arrayInt = [ 3, 2, 1, 6, 5, 10, 7, 4, 9, 8 ];
+
+console.log("Element in the 7th position: " + arrayInt.indexOf(7));
+
+console.log("\nElement in the 20th position: " + arrayInt.indexOf(20)); // If the element is not found, the value -1 will be returned.
