8db0eb5f8eee96f3eb1694d4374ac50eb76a4582
alanbrunoscience <alanbrunomath@gmail.com>
2024-12-09 12:58:43 -0300

    Insert Data Structure Algorithms

---
+++ 02_TypeScript/Class 03 - Data Structure/02_Stack/AppStack.ts
@@ -0,0 +1,20 @@
+/* Pilha
+
+O último elemento que entra é o primeiro a sair.
+
+*/
+
+import { Stack } from "./Stack";
+
+const stack = new Stack<string>();
+
+stack.push("Prato azul");
+stack.push("Prato verde");
+stack.push("Prato vermelho");
+
+stack.printStack();
+
+console.log("\nExiste um prato branco na pilha? ", stack.contains("Prato branco"));
+
+console.log();
+stack.printStack();
