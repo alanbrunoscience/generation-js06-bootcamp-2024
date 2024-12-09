b75cc59b6ffa0b67803ac3193ef23da845cab9b1
alanbrunoscience <alanbrunomath@gmail.com>
2024-12-09 09:44:41 -0300

    Insert Data Structure Algorithms

--- 02_TypeScript/Class 03 - Data Structure/01_Queue/AppQueue.ts
+++ 02_TypeScript/Class 03 - Data Structure/01_Queue/AppQueue.ts
@@ -6,7 +6,20 @@ const queue = new Queue<string>();
 queue.enqueue("Beatriz");
 queue.enqueue("Gustavo");
 queue.enqueue("Alana");
-queue.enqueue(readlinesync.question("Enter your name: "));
+// queue.enqueue(readlinesync.question("Enter your name: "));
 
 console.log();
 queue.printQueue();
+
+console.log();
+console.log(queue.contains("Alana")); // Verificar a existência de um elemento específico na fila.
+
+console.log();
+console.log(queue.count()); // Verificar o tamanho da fila, ou seja, a quantidade de elementos.
+
+console.log("\nElemento retirado: ", queue.dequeue()); // Retirar um elemento da fila. Remoção é sempre no início. O primeiro que entra é o primeiro a sair.
+
+console.log();
+queue.printQueue(); // Imprimir a fila.
+
+console.log("\nPróximo elemento a ser chamado: ", queue.peek()); // Mostrar o primeiro elemento da fila, ou seja, o próximo elemento a ser chamado.
