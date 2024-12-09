a8b3a52ebfa3689df56be3e02d383fb7f3396b57
alanbrunoscience <alanbrunomath@gmail.com>
2024-12-08 21:08:15 -0300

    Reorganize the code examples

--- 01_JavaScript/Class 06 - Loops/05_DoWhile.js
+++ 01_JavaScript/Class 06 - Loops/05_DoWhile.js
@@ -1,8 +1,11 @@
+"use strict"
+
 const input = require("readline-sync");
 
 let i = 1;
 let num = input.questionInt("Enter an integer number: ");
 
+console.log(`\n===== Multiplication Table of ${num} =====\n`);
 do {
     console.log(num + " * " + i + " = " + (num * i));
     i++;
