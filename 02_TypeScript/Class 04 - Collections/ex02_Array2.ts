da16eb0ef9bea75f7cc5172f725102c68d53d3f4
alanbrunoscience <alanbrunomath@gmail.com>
2024-12-09 18:06:45 -0300

    Insert TS examples codes

--- 02_TypeScript/Class 04 - Collections/ex02_Array2.ts
+++ 02_TypeScript/Class 04 - Collections/ex02_Array2.ts
@@ -25,13 +25,13 @@ console.table(numbers);
 
 
 // Searching for values ​​in the array
-console.log("\n-> Is there the number 5 on the array? ", numbers.includes(5)); // Checking if there is a specific number in the collection
+console.log("\n-> Is there the number 5 in the array? ", numbers.includes(5)); // Checking if there is a specific number in the collection
 
 console.log("\n-> What is the index of number 3 in the array? ", numbers.indexOf(3)); // Checking what the index of an element is
 
 
 // Deleting array elements
-numbers.splice(numbers.indexOf(10), 1); // Deleting ONE NUMBER from the collection. If, instead, "numbers.splice(numbers.indexOf(10), 1)" were placed "numbers.splice(numbers.indexOf(10), 2)", the element would be deleted, and the next
+numbers.splice(numbers.indexOf(10), 1); // Deleting ONE NUMBER of the collection. If, instead, "numbers.splice(numbers.indexOf(10), 1)" were placed "numbers.splice(numbers.indexOf(10), 2)", the element would be deleted, and the next
 
 console.log();
 console.table(numbers);
