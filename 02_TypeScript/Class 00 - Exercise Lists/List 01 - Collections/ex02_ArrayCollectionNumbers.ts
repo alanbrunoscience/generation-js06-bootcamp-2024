2f4b366b4f9200a92852422682dab8e63b224078
alanbrunoscience <alanbrunomath@gmail.com>
2024-12-09 21:23:53 -0300

    Insert TS examples codes

---
+++ 02_TypeScript/Class 00 - Exercise Lists/List 01 - Collections/ex02_ArrayCollectionNumbers.ts
@@ -0,0 +1,19 @@
+import readlineSync = require("readline-sync");
+
+// const numbers: Array<number> = new Array<number>(10);
+
+// console.log("======== Numeric Data Input ========\n");
+// for(let i = 0; i < numbers.length; i++) {
+//     numbers[i] = readlineSync.questionInt(`${i+1}) Enter an integer number: `, {limitMessage: "\n-> Invalid data type entered!\n"});
+// }
+
+const numbers: Array<number> = new Array<number>(2, 5, 1, 3, 4, 9, 7, 8, 10, 6);
+
+let num = readlineSync.questionInt("\nEnter the integer you want to find in the array collection: ", {limitMessage: "\n-> Invalid data type entered!"});
+
+let positionFound = numbers.indexOf(num);
+
+if (positionFound >= 0)
+    console.log(`\n-> The first occurrence of '${num}' in the array collection is in the position ${numbers.indexOf(num)}.`);
+else
+    console.log(`\n-> The number '${num}' was not found!`);
--- 02_TypeScript/Class 00 - Exercise Lists/List 01 - Collections/ex02_CollectionArrayNumbers.ts
+++
@@ -1,8 +0,0 @@
-import readlineSync = require("readline-sync");
-
-const numbers: Array<number> = new Array<number>(10);
-
-console.log("======== Numeric Data Input ========");
-for(let i = 0; i < numbers.length; i++) {
-    numbers[i] = readlineSync.questionInt(`\n-> Enter the ${i+1}º integer number: `, {limitMessage: "\n-> Invalid data type entered!"});
-}
