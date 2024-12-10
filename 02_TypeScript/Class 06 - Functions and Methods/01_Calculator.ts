85370034c33cde6b8b72923a031d0e6ef0640027
alanbrunoscience <alanbrunomath@gmail.com>
2024-12-10 09:44:33 -0300

    Insert code example of Function and Method

---
+++ 02_TypeScript/Class 06 - Functions and Methods/01_Calculator.ts
@@ -0,0 +1,92 @@
+import { get } from "http";
+import readlineSync = require("readline-sync");
+
+let number1: number;
+let number2: number;
+let option: number;
+
+do {
+
+    option = menu();
+
+    if(option === 0) {
+        console.log("\nFinishing the program...");
+        about();
+        process.exit(0);
+    }
+
+    switch(option) {
+        case 1:
+            // Desestruturação do Objeto
+            [number1, number2] = getNumbers();
+            console.log(`\n${number1} + ${number2} = ${add(number1, number2)}`);
+            break;
+        case 2:
+            [number1, number2] = getNumbers();
+            console.log(`\n${number1} - ${number2} = ${subtract(number1, number2)}`);
+            break;
+        case 3:
+            [number1, number2] = getNumbers();
+            console.log(`\n${number1} * ${number2} = ${multiply(number1, number2)}`);
+            break;
+        case 4:
+            [number1, number2] = getNumbers();
+
+            let result = divide(number1, number2);
+
+            if(result !== null) {
+                console.log(`\n${number1} / ${number2} = ${divide(number1, number2)}`);
+            } else {
+                console.log(`\n-> There is no division by zero!`);
+            }
+
+            break;
+        default:
+            console.log("\n-> Invalid option!");
+    }
+    
+} while(true);
+
+function menu(): number {
+
+    console.log("****************************");
+    console.log("         Calculator         ");
+    console.log("****************************");
+    console.log("\n1 - Add;");
+    console.log("2 - Subtract;");
+    console.log("3 - Multiply;");
+    console.log("4 - Divide;");
+    console.log("0 - Exit.");
+    console.log("\n****************************");
+    option = readlineSync.questionInt("\n-> Choose an option above: ", {limitMessage: "\n-> Invalid data type entered!"});
+
+    return option;
+
+} 
+
+function getNumbers(): [number, number] { 
+    const number1 = readlineSync.questionFloat("\nEnter the first integer: ", {limitMessage: "\n-> Invalid data type entered!"});
+    const number2 = readlineSync.questionFloat("\nEnter the second integer: ", {limitMessage: "\n-> Invalid data type entered!"});
+    return [number1, number2];
+}
+
+function add(number1: number, number2: number): number {
+    return number1 + number2;
+}
+
+function subtract(number1: number, number2: number): number {
+    return number1 - number2;
+}
+
+function multiply(number1: number, number2: number): number {
+    return number1 * number2;
+}
+
+function divide(number1: number, number2: number): number | null {
+    let division = number1 / number2;
+    return (division !== Infinity ? division: null);
+}
+
+function about() {
+    console.log("Created by Alan");
+}
