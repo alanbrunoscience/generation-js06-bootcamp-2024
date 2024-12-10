df8c70591c4e742f8a5486496d8ddf35a25446af
alanbrunoscience <alanbrunomath@gmail.com>
2024-12-10 01:26:49 -0300

    Insert TS examples codes

--- 02_TypeScript/Class 05 - Data Structure/02_Stack/AppStack1.ts
+++ 02_TypeScript/Class 05 - Data Structure/02_Stack/AppStack1.ts
@@ -1,6 +1,6 @@
 /* Pilha
 
-- Uma Pilha é uma estrutura de dados do tipo LIFO: "O último elemento inserido na pilha é o primeiro elemento que será retirado da pilha." (Last In - First Out).
+- Uma Pilha é uma estrutura de dados do tipo LIFO: "O último elemento inserido na pilha é o primeiro elemento que será retirado da pilha." (Last In - First Out), ou seja, um elemento sempre será inserido e retirado do topo da pilha.
 
 -> PUSH ou empurrar, insere um elemento no topo da pilha.
 -> POP ou Excluir, retira o elemento do topo da pilha (retornando-o ou não).
