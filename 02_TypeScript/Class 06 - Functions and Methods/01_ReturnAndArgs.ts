3b0350a6f1c0ce73ef49a1d34863582bcd76e424
alanbrunoscience <alanbrunomath@gmail.com>
2024-12-10 23:09:42 -0300

    Include Code Examples of Functions and Methods

---
+++ 02_TypeScript/Class 06 - Functions and Methods/01_ReturnAndArgs.ts
@@ -0,0 +1,12 @@
+// Function With Return And Argument
+
+let result: number;
+
+result = add(2, 2);
+
+console.log(`\n-> The result of add is ${result}.`);
+
+// Function Declaration
+function add(number1: number, number2: number): number {
+    return number1 + number2;
+}
