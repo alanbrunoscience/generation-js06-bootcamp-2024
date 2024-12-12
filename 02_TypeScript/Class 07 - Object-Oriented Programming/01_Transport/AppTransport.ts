ba1b75d39d11ad5f91a00c8d54b39a20b24b7d65
alanbrunoscience <alanbrunomath@gmail.com>
2024-12-12 11:09:57 -0300

    :sparkles: Adjust some details in the OOP example codes

--- 02_TypeScript/Class 07 - Object-Oriented Programming/01_Transport/AppTransport.ts
+++ 02_TypeScript/Class 07 - Object-Oriented Programming/01_Transport/AppTransport.ts
@@ -4,13 +4,13 @@ import { Car } from "./Car";
 
 export function main() {
 
-    const t1: Transport = new Transport(5);
+    const t1 = new Transport(5); // const t1: Transport = new Transport(5); (Explicit declaration)
     t1.display();
 
-    const tr1: Land = new Land(5, 4, 100);
+    const tr1 = new Land(5, 4, 100);
     tr1.display();
 
-    const a1: Car = new Car(5, 4, 250, "Green", 5, "FLV-4566", 5);
+    const a1 = new Car(5, 4, 250, "Green", 5, "FLV-4566", 5);
     a1.display();
 }
 
