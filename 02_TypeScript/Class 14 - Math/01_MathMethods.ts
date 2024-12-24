afcb0c941c22a54f0c3fa707fe3dddfd275f3109
alanbrunoscience <alanbrunomath@gmail.com>
2024-12-24 12:55:14 -0300

    :sparkles: Include code examples from 'Class 14 - Math'

--- 02_TypeScript/Class 14 - Math/01_MathMethods.ts
+++ 02_TypeScript/Class 14 - Math/01_MathMethods.ts
@@ -1,12 +1,24 @@
 console.log("========================= MATH METHODS =========================\n");
 
-console.log("1) Math.abs(x):\n\n- Returns the absolute value of the number (without signal):");
+console.log("1) Math.E:\n\n- Represents Euler's constant and also the base of natural logarithms:");
+
+console.log(`\n-> Math.E: ${Math.E}.`);
+
+console.log("\n================================================================\n");
+
+console.log("2) Math.PI:\n\n- Returns the value of PI:");
+
+console.log(`\n-> Math.PI: ${Math.PI}.`);
+
+console.log("\n================================================================\n");
+
+console.log("3) Math.abs(x):\n\n- Returns the absolute value of the number (without signal):");
 
 console.log(`\n-> Math.abs(-10): ${Math.abs(-10)}.`);
 
 console.log("\n================================================================\n");
 
-console.log("2) Math.sign(x):\n\n- Returns the signal of 'x', indicating if it is a positive (1),\nnegative (-1), or zero (0) number:");
+console.log("4) Math.sign(x):\n\n- Returns the signal of 'x', indicating if it is a positive (1),\nnegative (-1), or zero (0) number:");
 
 console.log(`\n-> Math.sign(10): ${Math.sign(10)};`);
 console.log(`\n-> Math.sign(0): ${Math.sign(0)};`);
@@ -14,50 +26,56 @@ console.log(`\n-> Math.sign(-10): ${Math.sign(-10)}.`);
 
 console.log("\n================================================================\n");
 
-console.log("3) Math.pow(x, y):\n\n- Returns the base 'x' raised to the power 'y':");
+console.log("5) Math.pow(x, y):\n\n- Returns the base 'x' raised to the power 'y':");
 
 console.log(`\n-> Math.pow(2.0, 7.0): ${Math.pow(2.0, 7.0).toFixed(2)}.`);
 
 console.log("\n================================================================\n");
 
-console.log("4) Math.sqrt(x):\n\n- Returns the positive square root of a number:");
+console.log("6) Math.sqrt(x):\n\n- Returns the positive square root of a number:");
 
 console.log(`\n-> Math.sqrt(900.0): ${Math.sqrt(900.0).toFixed(2)}.`);
 
 console.log("\n================================================================\n");
 
-console.log("5) Math.cbrt(x):\n\n- Returns the cube root of a number:");
+console.log("7) Math.cbrt(x):\n\n- Returns the cube root of a number:");
 
 console.log(`\n-> Math.cbrt(27.0): ${Math.cbrt(27.0).toFixed(2)}.`);
 
 console.log("\n================================================================\n");
 
-console.log("6) Math.log(x):\n\n- Returns the natural logarithm of a number:");
+console.log("8) Math.log(x):\n\n- Returns the natural logarithm of a number:");
 
 console.log(`\n-> Math.log(2.718282): ${Math.log(2.718282).toFixed(2)}.`);
 
 console.log("\n================================================================\n");
 
-console.log("7) Math.sin(x):\n\n- Returns the sine of a number:");
+console.log("9) Math.sin(x):\n\n- Returns the sine of a number:");
 
 console.log(`\n-> Math.sin(0.0): ${Math.sin(0.0).toFixed(2)}.`);
 
 console.log("\n================================================================\n");
 
-console.log("8) Math.cos(x):\n\n- Returns the cosine of a number:");
+console.log("10) Math.cos(x):\n\n- Returns the cosine of a number:");
 
 console.log(`\n-> Math.cos(0.0): ${Math.cos(0.0).toFixed(2)}.`);
 
 console.log("\n================================================================\n");
 
-console.log("9) Math.ceil(x):\n\n- Returns the smallest integer that is greater than or equal to\na number:");
+console.log("11) Math.tan(x):\n\n- Returns the tangent of a number:");
+
+console.log(`\n-> Math.tan(0.0): ${Math.tan(0.0).toFixed(2)}.`);
+
+console.log("\n================================================================\n");
+
+console.log("12) Math.ceil(x):\n\n- Returns the smallest integer that is greater than or equal to\na number:");
 
 console.log(`\n-> Math.ceil(9.2): ${Math.ceil(9.2).toFixed(2)};`);
 console.log(`\n-> Math.ceil(-9.8): ${Math.ceil(-9.8).toFixed(2)}.`);
 
 console.log("\n================================================================\n");
 
-console.log("10) Math.round(x):\n\n- Returns the rounded value of 'x', to the nearest integer value:");
+console.log("13) Math.round(x):\n\n- Returns the rounded value of 'x', to the nearest integer value:");
 
 console.log(`\n-> Math.round(9.4): ${Math.round(9.4).toFixed(2)};`);
 console.log(`\n-> Math.round(9.5): ${Math.round(9.5).toFixed(2)};`);
@@ -65,34 +83,34 @@ console.log(`\n-> Math.round(-9.5): ${Math.round(-9.5).toFixed(2)}.`);
 
 console.log("\n================================================================\n");
 
-console.log("11) Math.floor(x):\n\n- Returns the largest integer that is less than or equal to a\nnumber:");
+console.log("14) Math.floor(x):\n\n- Returns the largest integer that is less than or equal to a\nnumber:");
 
 console.log(`\n-> Math.floor(9.2): ${Math.floor(9.2).toFixed(2)};`);
 console.log(`\n-> Math.floor(-9.8): ${Math.floor(-9.8).toFixed(2)}.`);
 
 console.log("\n================================================================\n");
 
-console.log("12) Math.trunc(x):\n\n- Returns the integer part of 'x', removing any fractional\ndigits:");
+console.log("15) Math.trunc(x):\n\n- Returns the integer part of 'x', removing any fractional\ndigits:");
 
 console.log(`\n-> Math.trunc(9.4): ${Math.trunc(9.4)};`);
 console.log(`\n-> Math.trunc(-8.0): ${Math.trunc(-8.0)}.`);
 
 console.log("\n================================================================\n");
 
-console.log("13) Math.max(n1, n2, ..., n):\n\n- Returns the largest of the received parameters:");
+console.log("16) Math.max(n1, n2, ..., n):\n\n- Returns the largest of the received parameters:");
 
 console.log(`\n-> Math.max(2.3, 12.7): ${Math.max(2.3, 12.7)};`);
 console.log(`\n-> Math.max(-2.3, -12.7): ${Math.max(-2.3, -12.7)}.`);
 
 console.log("\n================================================================\n");
 
-console.log("14) Math.min(n1, n2, ..., n):\n\n- Returns the smallest of the received parameters:");
+console.log("17) Math.min(n1, n2, ..., n):\n\n- Returns the smallest of the received parameters:");
 
 console.log(`\n-> Math.min(2.3, 12.7): ${Math.min(2.3, 12.7)};`);
 console.log(`\n-> Math.min(-2.3, -12.7): ${Math.min(-2.3, -12.7)}.`);
 
 console.log("\n================================================================\n");
 
-console.log("15) Math.random():\n\n- Returns a random number between 0 and 1:");
+console.log("18) Math.random():\n\n- Returns a random number between 0 and 1:");
 
 console.log(`\n-> Math.random(): ${Math.random().toFixed(2)}.`);
